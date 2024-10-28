a4fec9dbe0264950a5b0aeef180f5d1a8435d853
Tkyou <stoa806@handong.ac.kr>
2024-10-28 11:00:35 +0900

    First!

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './Component1';
+import Component001 from './Component001';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          20241028 react oss class
+        </p>
+        <Component1 />
+        <Component001 />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component1.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Component1 extends Component {
+  render() {
+    return (
+        //rcc하면 이렇게 나와!
+      <div>Component1: react!!!</div>
+    )
+  }
+}
